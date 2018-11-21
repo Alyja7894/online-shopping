922ecc8ce2aa9dbb48b2b0ee2f9d3998f2faecf5
Alyja <alyja7894@gmail.com>
2018-11-21 11:11:54 +0100

    Completed the interface theme and other issues!

--- onlineshopping/src/main/java/com/alyja/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/alyja/onlineshopping/controller/PageController.java
@@ -13,10 +13,48 @@ public class PageController {
 	public ModelAndView index(){
 		
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring Web MVC");
+		//mv.addObject("greeting", "Welcome to Spring Web MVC");
+		
+		mv.addObject("tittle", "Home");
+		mv.addObject("userClickHome", true);/* */
 		
 		return mv;
 	}
+	@RequestMapping(value= {"/about"})
+	public ModelAndView about(){
+		
+		ModelAndView mv = new ModelAndView("page");
+		//mv.addObject("greeting", "Welcome to Spring Web MVC");
+		
+		mv.addObject("tittle", "About Us");
+		mv.addObject("userClickAbout", true);/* */
+		
+		return mv;
+	}
+	@RequestMapping(value= {"/contact"})
+	public ModelAndView contact(){
+		
+		ModelAndView mv = new ModelAndView("page");
+		//mv.addObject("greeting", "Welcome to Spring Web MVC");
+		
+		mv.addObject("tittle", "Contact Us");
+		mv.addObject("userClickContact", true);/* */
+		
+		return mv;
+	} 
+	
+	@RequestMapping(value= {"/listProducts"})
+	public ModelAndView listProducts(){
+		
+		ModelAndView mv = new ModelAndView("page");
+		//mv.addObject("greeting", "Welcome to Spring Web MVC");
+		
+		mv.addObject("tittle", "listProducts Us");
+		mv.addObject("userClicklistProducts", true);/* */
+		
+		return mv;
+	} 
+	
 	//test?greeting=Welcome%20Albert -> "/onlineshopping says- Welcome Albert"
 	/*@RequestMapping(value="/test")
 	public ModelAndView test(@RequestParam(value="greeting",required = false)String greeting) {
---
+++ onlineshopping/src/main/webapp/WEB-INF/views/listProducts.jsp
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class = "row">
+		<h1>List Products Us</h1>
+		<hr/>
+		<p>This is the List products page</p>
+	</div>
+
+</div>
